98ed38b28fdf61dcc5e122c65ef1e870d397e776
buaretti <marcelobuaretti@gmail.com>
2023-09-17 08:02:12 -0300

    page2

--- scripts/script.js
+++ scripts/script.js
@@ -41,56 +41,74 @@ document.addEventListener("DOMContentLoaded", function () {
   var range = document.getElementById("range");
   var m = document.getElementById("m");
   var y = document.getElementById("y");
-  
+  var arcadeMonth = document.getElementById("arcademonth");
+  var advancedMonth = document.getElementById("advancedmonth");
+  var proMonth = document.getElementById("promonth");
+
   function setColors() {
-      if (range.value == 0) {
-          m.style.color = "hsl(213, 96%, 18%)";
-          y.style.color = "hsl(231, 11%, 63%)";
-        } else if (range.value == 1) {
-            m.style.color = "hsl(231, 11%, 63%)";
-            y.style.color = "hsl(213, 96%, 18%)";
-            
-        }
+    if (range.value == 0) {
+      m.style.color = "hsl(213, 96%, 18%)";
+      y.style.color = "hsl(231, 11%, 63%)";
+
+      arcadeMonth.textContent = "$9/mo";
+      advancedMonth.textContent = "$12/mo";
+      proMonth.textContent = "$15/mo";
+    } else if (range.value == 1) {
+      m.style.color = "hsl(231, 11%, 63%)";
+      y.style.color = "hsl(213, 96%, 18%)";
+      arcadeMonth.textContent = "$90/yr";
+      advancedMonth.textContent = "$120/yr";
+      proMonth.textContent = "$150/yr";
     }
-    
+  }
+
+  setColors();
+
+  range.addEventListener("input", function () {
     setColors();
-    
-    range.addEventListener("input", function () {
-        setColors();
-    });
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    var range = document.getElementById("range");
-    var monthsfree = document.querySelectorAll(".monthsfree");
-  
-    function setDisplay() {
-      if (range.value == 0) {
-        monthsfree.forEach(function (element) {
-          element.style.display = "none";
-        });
-      } else if (range.value == 1) {
-        monthsfree.forEach(function (element) {
-          element.style.display = "block";
-        });
-      }
+  var range = document.getElementById("range");
+  var monthsfree = document.querySelectorAll(".monthsfree");
+
+  function setDisplay() {
+    if (range.value == 0) {
+      monthsfree.forEach(function (element) {
+        element.style.display = "none";
+      });
+    } else if (range.value == 1) {
+      monthsfree.forEach(function (element) {
+        element.style.display = "block";
+      });
     }
-  
+  }
+
+  setDisplay();
+
+  range.addEventListener("input", function () {
     setDisplay();
-  
-    range.addEventListener("input", function () {
-      setDisplay();
-    });
-  });  
-
-const currentPage = 1;
-
-const circles = document.querySelectorAll(".circle");
-circles.forEach((circle) => {
-  circle.classList.remove("active");
+  });
 });
 
-const currentCircle = document.getElementById(`n${currentPage}`);
-currentCircle.classList.add("active");
+const path = window.location.pathname;
 
-function nextPage() {}
+const match = path.match(/page(\d+)\.html/);
+
+if (match) {
+  const currentPage = parseInt(match[1], 10);
+  
+  const circles = document.querySelectorAll(".circle");
+  circles.forEach((circle, index) => {
+    if (index + 1 === currentPage) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  
+  console.log("Página atual: " + currentPage);
+} else {
+  console.log("Página não encontrada ou não possui um número.");
+}
